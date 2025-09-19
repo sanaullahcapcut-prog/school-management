6ad69204fec10c5311dfff7a9fe30326b74251f3
sanaullahcapcut-prog <sanaullahcapcut@gmail.com>
2025-09-19 11:58:23 -0700

    Create main.tsx

---
+++ src/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OnlineTest from "./OnlineTest";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <OnlineTest />
+  </React.StrictMode>
+);
